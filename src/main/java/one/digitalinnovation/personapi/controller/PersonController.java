5b4899b9662470686a2e2000735b09ed9e95bae0
Erik Ribeiro <61017426+ErikRibeiro@users.noreply.github.com>
2021-07-14 22:21:01 -0300

    Add files via upload

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API Test!";
+    }
+}
